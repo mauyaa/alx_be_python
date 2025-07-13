4a51b32d92101e0152d53010726c76e943a13797
mauyaa <mauyabevan@gmail.com>
2025-07-13 23:23:38 +0300

    Update hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,1 +1,10 @@
-"" 
+# hours_to_seconds.py
+
+# Assign number of hours
+hours = 2
+
+# Convert hours to seconds
+seconds = hours * 3600
+
+# Print result
+print(hours, "hour(s) is", seconds, "seconds.")
