fd5a3b03d99e43b2041996937b2b9f76583a71a3
mauyaa <mauyabevan@gmail.com>
2025-07-13 23:21:34 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,1 +1,13 @@
-"" 
+# simple_interest.py
+
+# Assign values
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculate simple interest
+interest = principal * rate * time
+
+# Print result
+print("The simple interest is:", interest)
+
